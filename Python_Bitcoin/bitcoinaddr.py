7e13f29fd8db089da69f83bc563766d3f4ba1615
jinsung choi <js@localhost.localdomain>
2017-07-19 04:01:02 +0900

    add Python_Bitcoin

---
+++ Python_Bitcoin/bitcoinaddr.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+import ecdsa
+import base58
+import hashlib
+
+
+def privkeytopubkey(privk):
+    sk = ecdsa.SigningKey.from_string(privk, curve=ecdsa.SECP256k1)
+    pubk = b'\04' + sk.verifying_key.to_string()
+    return pubk
+
+
+def pubkeytoaddr(pubk):
+    hash1 = hashlib.sha256(pubk).digest()
+    hash2 = hashlib.new('ripemd160', data=hash1).digest()
+    addr = base58.b58encode_check(hash2)
+    return addr
+
+
+if __name__ == '__main__':
+    privkey = bytes.fromhex('18E14A7B6A307F426A94F8114701E7C8E774E7F9A47E2C2035DB29A206321725')
+
+    pubkey = privkeytopubkey(privkey)
+    print('pubkey: {}'.format(pubkey.hex()))
+
+    addr = pubkeytoaddr(pubkey)
+    print('addr: {}'.format(addr))
