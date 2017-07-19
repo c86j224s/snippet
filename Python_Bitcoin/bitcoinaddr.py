a14f4dc8bef011a5e48dc5caecb6f254d2a18534
jinsung choi <js@localhost.localdomain>
2017-07-19 17:56:51 +0900

    update bitcoinaddr.py

--- Python_Bitcoin/bitcoinaddr.py
+++ Python_Bitcoin/bitcoinaddr.py
@@ -9,7 +9,6 @@ import unittest
 
 
 # ref : https://en.bitcoin.it/wiki/Technical_background_of_version_1_Bitcoin_addresses
-# ref :https://davanum.wordpress.com/2014/03/17/generating-a-bitcoin-private-key-and-address/
 
 
 def privkeytopubkey(privk):
