8a69e8532cf7755b2c67f29df79fb8310edbd431
allthatcode <allthatcode@allthatcodeui-MacBook-Pro.local>
2017-11-10 19:41:13 +0900

    Rust_TcpEcho: upgrade to support multiple connections

--- Rust_TcpEcho/echo/src/main.rs
+++ Rust_TcpEcho/echo/src/main.rs
@@ -1,5 +1,6 @@
 use std::net::{TcpListener};
 use std::io::{BufReader, BufWriter, BufRead, Write};
+use std::thread;
 
 fn main() {
     let listener = match TcpListener::bind("127.0.0.1:8888") {
@@ -7,34 +8,51 @@ fn main() {
         Err(e) => { panic!("bind failed : {:?}", e) }
     };
 
+    let mut threads = vec![];
+
     for stream in listener.incoming() {
         let client = match stream {
             Ok(v) => { println!("new client accepted! {:?}", v.peer_addr().unwrap()); v },
                 Err(e) => { panic!("accept failed : {:?}", e); }
         };
 
-        let mut reader = BufReader::new(&client);
-        let mut writer = BufWriter::new(&client);
+        let handle = thread::spawn(move || {
+            let mut reader = BufReader::new(&client);
+            let mut writer = BufWriter::new(&client);
 
-        let mut line = String::new();
-        loop {
-            match reader.read_line(&mut line) {
-                Ok(len) => { 
-                    println!("read : {} chars, {}", len, line);
-                    len
-                },
-                Err(e) => {
-                    println!("read error! {:?}", e);
-                    break;
+            let mut line = String::new();
+            loop {
+                match reader.read_line(&mut line) {
+                    Ok(_) => { 
+                        println!("[{:?}] {}", 
+                                client.peer_addr().unwrap(), 
+                                line.trim());
+                    },
+                    Err(e) => {
+                        println!("[LOG][ERROR] read error! {:?}", e);
+                        break;
+                    }
+                };
+
+                write!(writer, "{}", line);
+                match writer.flush() {
+                    Err(e) => { println!("[LOG][ERROR] flush error. {:?}", e); },
+                    _ => {}
                 }
-            };
 
-            write!(writer, "{}", line);
-            writer.flush();
+                line.clear();
+            }
 
-            line.clear();
-        }
+            println!("client disconnected. {:?}", client.peer_addr().unwrap());
+        });
+
+        threads.push(handle);
     }
 
-
+    for each in threads {
+        match each.join() {
+            Err(e) => { println!("[LOG][ERROR] thread join error. {:?}", e); },
+            _ => {}
+        }
+    }
 }
