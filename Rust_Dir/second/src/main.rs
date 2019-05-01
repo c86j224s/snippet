cfd8e574bd234031dc1d3bbb56edba4e5a9e300c
js <c86j224s@gmail.com>
2019-05-01 23:33:01 +0900

    Rust_dir/second

--- Rust_Dir/second/src/main.rs
+++ Rust_Dir/second/src/main.rs
@@ -1,5 +1,9 @@
 use std::fs;
+use std::io;
+use std::result::Result;
 use std::io::prelude::*;
+
+use regex;
 
 
 /*
@@ -10,21 +14,67 @@ use std::io::prelude::*;
 한글이름(EnglishName) <email@addr.ess>
 한글이름(EnglishName) <email@addr.ess>
 */
-fn main() {
-    let mut file = fs::File::open("test.txt").unwrap();
+fn test1() -> Result<(), io::Error> {
+    println!("* test1 * ");
+
+    let mut file = fs::File::open("test.txt")?;
 
     let mut txt = String::new();
     file.read_to_string(&mut txt).unwrap();
 
     for line in txt.split(|c| c == '\r' || c == '\n') {
+        if line.len() < 1 {
+            continue;
+        }
+
         let mut splitted = line.split(|c| c == '<' || c == '>');
 
-        let name = splitted.next().unwrap().trim();
-        let email = splitted.next().unwrap().trim();
+        let name = match splitted.next() {
+            Some(some) => some,
+            _ => { panic!("no name");  }
+        };
+        let name = name.trim();
+        
+        let email = match splitted.next() {
+            Some(some) => some,
+            _ => { panic!("no email"); }
+        };
+        let email = email.trim();
 
         println!("name[{}], email[{}]", name, email);
     }
 
+    Ok(())
+}
 
-    println!("Hello, world!");
+fn test2() -> Result<(), io::Error> {
+    println!("* test2 *");
+
+    let mut file = fs::File::open("test.txt")?;
+
+    let mut txt = String::new();
+    file.read_to_string(&mut txt).unwrap();
+
+    let re = match regex::Regex::new(r"(\w+\([\w\s]+\)) <(\w+@\w+\.\w+)>") {
+        Ok(ok) => ok,
+        Err(err) => { println!("{}", err); panic!(""); }
+    };
+    for line in txt.split(|c| c == '\r' || c == '\n') {
+        if line.len() < 1 {
+            continue;
+        }
+
+        for cap in re.captures_iter(line) {
+            println!("name[{0}], email[{1}]", &cap[1], &cap[2]);
+        }
+    }
+
+    Ok(())
+}
+
+
+fn main() {
+    test1().expect("");
+    test2().expect("");
+
 }
