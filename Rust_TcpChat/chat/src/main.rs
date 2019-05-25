2794932007d6d825af4833b7d345251c02681f67
js <c86j224s@gmail.com>
2019-05-25 11:45:03 +0900

    Rust_TcpChat: completed.

--- Rust_TcpChat/chat/src/main.rs
+++ Rust_TcpChat/chat/src/main.rs
@@ -1,3 +1,10 @@
+/*
+ *  Rust std reference만 보고 채팅 서버 만들기 도전!!
+ *
+ *  2017.11.14. 처음 시작해서 조금씩 해봄... 거의 완성.
+ *  2019.05.25. 다시 꺼내서.. 이제 완료?
+ */
+
 //use std::net::{TcpListener};
 use std::net::{TcpListener, TcpStream};
 use std::io::{BufReader, BufWriter, BufRead, Write};
@@ -5,18 +12,25 @@ use std::thread;
 use std::sync::{Arc, Mutex};
 
 fn main() {
-    let listener = match TcpListener::bind("127.0.0.1:8888") {
+    // 리스너 바인딩.
+    let listener : TcpListener = match TcpListener::bind("127.0.0.1:8888") {
         Ok(v) => { println!("bound on 127.0.0.1:8888"); v },
         Err(e) => { panic!("binding failed. {:?}", e) }
     };
 
+    // 쓰레드들의 핸들을 종료 시에 Join 하기 위해 관리해준다.
     let mut threads = vec![];
+
+    // 클라이언트들(TcpStream)을 관리해준다. 나중에 이걸로 채팅을 뿌려줘야 한다.
+    // @TODO: 아래는 최종 코드에서는 지워야 할 주석임. 만들면서 중간의 여러 삽질들...
     //let mut clients_vec = vec![];
     //let clients : Arc<&mut Vec<Arc<TcpStream>>> = Arc::new(&mut clients_vec);
     let clients = Arc::new(Mutex::new(vec![]));
 
+    // 들어오는 커넥션들을 받아준다. 계속 들어올 수 있으므로 루프 돈다.
     for stream in listener.incoming() {
-        let client = Arc::new(match stream {
+        // 방금 들어온 커넥션의 TcpStream을 레퍼런스 카운터로 감싸준다.
+        let client : Arc<TcpStream> = Arc::new(match stream {
             Ok(v) => {
                 println!("new client accept! {:?}", v.peer_addr().unwrap());
                 v 
@@ -24,40 +38,47 @@ fn main() {
             Err(e) => { panic!("accepting failed. {:?}", e) }
         });
 
-        {
-            //Arc::make_mut(&mut clients).unwrap().push(Arc::clone(&client));
-            clients.lock().unwrap().push(Arc::clone(&client));
-        }
+        // 전체 클라이언트 커넥션 목록에 방금 들어온 커넥션을 추가해준다.
+        //Arc::make_mut(&mut clients).unwrap().push(Arc::clone(&client));
+        clients.lock().unwrap().push(Arc::clone(&client));
 
+        // 방금 들어온 커넥션의 IO 쓰레드를 만들어준다. 커넥션들은 레퍼런스 카운터 올려서 넘겨준다.
         let thread_client = Arc::clone(&client);
         let thread_clients = Arc::clone(&clients);
+        // handle : JoinHandle<T>
         let handle = thread::spawn(move || {
+            // BufReader로 받아서 수신 루프 돌려줌.
             let mut reader = BufReader::new(&*thread_client);
+            let peer = thread_client.peer_addr().unwrap();
 
             loop {
+                // 읽어서.. 서버에 뿌려주고..
                 let mut line = String::new();
-                let peer = match reader.read_line(&mut line) {
+                match reader.read_line(&mut line) {
                     Ok(_) => {
-                        let peer = Some(thread_client.peer_addr().unwrap());
-                        println!("[{:?}] {}",
-                            peer,
-                            line.trim()
-                        );
-                        peer
+                        println!("[{:?} said] {}", peer, line.trim());
                     },
                     Err(e) => {
-                        println!("Error - [{:?}] read error. {:?}", 
-                            thread_client.peer_addr().unwrap(),
-                            e
-                        );
+                        println!("[{:?} error] read error. {:?}", peer, e);
+
+                        // read_line에서 실패했으면, 그냥 끊어버린다.
+                        let idx = thread_clients
+                            .lock()
+                            .unwrap()
+                            .iter()
+                            .position(|r| (*r).peer_addr().unwrap() == thread_client.peer_addr().unwrap())
+                            .unwrap();
+                        thread_clients.lock().unwrap().remove(idx);
+                        println!("[{:?} error] server disconnected", peer);
                         break;
                     }
                 };
 
+                // 다른 피어들에게 뿌려준다.
                 for it in thread_clients.lock().unwrap().iter() {
-                    let cli = Arc::clone(&it);
-                    let mut writer = BufWriter::new(&*cli);
-                    write!(writer, "[{:?}] {}", peer, line);
+                    let another_peer = Arc::clone(&it);
+                    let mut writer = BufWriter::new(&*another_peer);
+                    write!(writer, "[{:?} said] {}", peer, line);
                     match writer.flush() {
                         Err(e) => println!("Error - flush error. {:?}", e),
                         _ => {}
@@ -67,12 +88,14 @@ fn main() {
                 line.clear();
             }
             
-            println!("[{:?}] disconnected.", thread_client.peer_addr().unwrap());
+            println!("[{:?} event] disconnected.", thread_client.peer_addr().unwrap());
         });
 
+        // IO 쓰레드는 전체 쓰레드 핸들 목록에 추가해줌..
         threads.push(handle);
     }
 
+    // 종료할 땐 다 잘 닫아주자..
     for each in threads {
         match each.join() {
             Err(e) => { println!("Error - thread join error. {:?}", e) },
