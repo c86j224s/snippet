fcaa538c838c6625c2df374665f6b7694d6b36fd
js <c86j224s@gmail.com>
2019-06-11 22:55:59 +0900

    Rust_TcpChat/chat2: added

---
+++ Rust_TcpChat/chat2/target/debug/chat2.d
@@ -0,0 +1,1 @@
+/home/adodajs/github/c86j224s/snippet/Rust_TcpChat/chat2/target/debug/chat2: /home/adodajs/github/c86j224s/snippet/Rust_TcpChat/chat2/src/main.rs
---
+++ Rust_TcpChat/chat2/target/debug/deps/chat2-621759feaac5e716.d
@@ -0,0 +1,5 @@
+/home/adodajs/github/c86j224s/snippet/Rust_TcpChat/chat2/target/debug/deps/chat2-621759feaac5e716: src/main.rs
+
+/home/adodajs/github/c86j224s/snippet/Rust_TcpChat/chat2/target/debug/deps/chat2-621759feaac5e716.d: src/main.rs
+
+src/main.rs:
