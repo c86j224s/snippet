afe24ef92f811dd715c156b679e4ab2b72c501f8
js <c86j224s@gmail.com>
2019-12-28 01:20:24 +0900

    Rust_Tide: resolved the 'future is not send' problem.'

--- Rust_Tide/todo/src/main.rs
+++ Rust_Tide/todo/src/main.rs
@@ -66,13 +66,17 @@ impl GlobalStorage {
         }
     }
 
-
     async fn delay(&self) {
         println!("delaying...");
         task::sleep(Duration::from_secs(3)).await;
         println!("done...");
     }
 
+    async fn get_static_async(&self, input_path: &Path) -> std::io::Result<Arc<Mutex<String>>> {
+        let mut sfs = { self.database.lock().unwrap().static_file_server.clone() };
+
+        sfs.get_cached_file(input_path).await
+    }
 }
 
 async fn handle_msg_get(req: Request<GlobalStorage>) -> Response {
@@ -126,7 +130,33 @@ async fn handle_delay(req: Request<GlobalStorage>) -> Response {
     Response::new(StatusCode::OK.into()).body_string("ok.".to_owned())
 }
 
+async fn handle_get_static(req: Request<GlobalStorage>) -> Response {
+    // todo: fix verifying rules...
+    let input_path_buf = req.uri().path().to_owned();
+    let regined_path_string = format!("./static_files/{}", &input_path_buf[8..]);
+    let rel_path = Path::new(&regined_path_string);
+
+    let cloned_state = {
+        req.state().clone()
+    };
+
+    match cloned_state.get_static_async(&rel_path).await {
+        Ok(v) => {
+            Response::new(StatusCode::OK.into()).body_string(v.lock().unwrap().to_owned())
+        },
+        Err(e) => {
+            println!("get static async failed. {:#?}", e);
+            Response::new(StatusCode::INTERNAL_SERVER_ERROR.into())
+        }
+    }
+}
+
+
 struct StaticFileHandler {
+    inner: Arc<Mutex<StaticFileHandlerInner>>
+}
+
+struct StaticFileHandlerInner {
     root_path: PathBuf,
     file_cache: HashMap<PathBuf, Arc<Mutex<String>>>
 }
@@ -136,74 +166,94 @@ impl StaticFileHandler {
         let abs_root_path = fs::canonicalize(root_path).await?;
 
         Ok(StaticFileHandler {
-            root_path : abs_root_path,
-            file_cache : Default::default()
+            inner: Arc::new(Mutex::new(StaticFileHandlerInner {
+                root_path : abs_root_path,
+                file_cache : Default::default()
+            }))
         })
     }
 
-/*
+    fn clone(&self) -> StaticFileHandler {
+        StaticFileHandler {
+            inner: self.inner.clone()
+        }
+    }
+
+    fn get_root_path(&self) -> PathBuf {
+        self.inner.lock().unwrap().root_path.clone()
+    }
+
+    fn contains(&self, k: &Path) -> bool {
+        self.inner.lock().unwrap().file_cache.contains_key(k)
+    }
+
+    fn update(&mut self, k: PathBuf, v: String) {
+        self.inner.lock().unwrap().file_cache.entry(k).and_modify(|e| *e = Arc::new(Mutex::new(v)));
+    }
+    
+    fn insert(&mut self, k: PathBuf, v: String) {
+        self.inner.lock().unwrap().file_cache.insert(k, Arc::new(Mutex::new(v)));
+    }
+
+    fn get_cached(&self, k: &Path) -> Option<Arc<Mutex<String>>> {
+        match self.inner.lock().unwrap().file_cache.get(k) {
+            Some(v) => Some(v.clone()),
+            None => None
+        }
+    }
+
     async fn check_valid_path(&self, input_path: &Path) -> std::io::Result<bool> {
+        let cloned = { self.clone() };
+
         let abs_input_path = fs::canonicalize(input_path).await?;
 
-        Ok(abs_input_path.as_path().starts_with(&self.root_path))
+        Ok(abs_input_path.as_path().starts_with(&cloned.get_root_path()))
     }
 
     async fn cache_file(&mut self, input_path: &Path) -> std::io::Result<bool> {
-        debug_assert!(self.check_valid_path(input_path).await?);
+        let mut cloned = { self.clone() };
+
+        debug_assert!(cloned.check_valid_path(input_path).await?);
 
         // 1. load file
-        let new_file_data = Arc::new(Mutex::new(fs::read_to_string(input_path).await?));
+        let new_file_data = fs::read_to_string(input_path).await?;
 
         // 2. build key
         let abs_input_path = fs::canonicalize(input_path).await?;
 
         // 3. set key value
-        if self.file_cache.contains_key(&abs_input_path) {
-            self.file_cache.entry(abs_input_path).and_modify(|e| *e = new_file_data);
+        if cloned.contains(&abs_input_path) {
+            cloned.update(abs_input_path, new_file_data);
         }
         else {
-            self.file_cache.insert(abs_input_path, new_file_data);
+            cloned.insert(abs_input_path, new_file_data);
         }
 
         Ok(true)
     }
 
-    async fn get_cached_file(&self, input_path: &Path) -> std::io::Result<Arc<Mutex<String>>> {
+    async fn get_cached_file(&mut self, input_path: &Path) -> std::io::Result<Arc<Mutex<String>>> {
+        let mut cloned = { self.clone() };
+
         if !self.check_valid_path(input_path).await? {
             return Err(std::io::Error::from(std::io::ErrorKind::NotFound))
         }
 
         let abs_input_path = fs::canonicalize(input_path).await?;
 
-        if !self.file_cache.contains_key(&abs_input_path) {
-            self.cache_file(&abs_input_path).await?;
+        if !cloned.contains(&abs_input_path) {
+            cloned.cache_file(&abs_input_path).await?;
+            println!("cached the file : {:?}", abs_input_path);
         }
         
-        match self.file_cache.get(&abs_input_path) {
+        match cloned.get_cached(&abs_input_path) {
             Some(v) => Ok(v.clone()),
             None => Err(std::io::Error::from(std::io::ErrorKind::NotFound))
         }
     }
-    */
 }
 
 
-/*
-async fn handle_get_static(req: Request<GlobalStorage>) -> Response {
-    let rel_path_buf = req.uri().path().to_owned();
-    let rel_path = Path::new(&rel_path_buf[7..]);
-
-    let mut cloned_database = {
-        req.state().database.clone()
-    };
-
-    match cloned_database.lock().unwrap().static_file_server.get_cached_file(&rel_path).await {
-        //Ok(v) => Response::new(StatusCode::OK.into()).body_string(*v),
-        Ok(v) => Response::new(StatusCode::OK.into()),
-        _ => Response::new(StatusCode::INTERNAL_SERVER_ERROR.into())
-    }
-}
-*/
 
 
 #[async_std::main]
@@ -212,10 +262,9 @@ async fn main() -> std::io::Result<()> {
 
     let mut app = Server::with_state(database);
     app.at("/").get(|_| async move { "Hello, world!" });
-    //app.at("/message/:id").post(handle_post).get(handle_get);
     app.at("/msg/:id").post(handle_msg_post).get(handle_msg_get);
     app.at("/delay").get(handle_delay);
-    //app.at("/static/*").get(handle_get_static);
+    app.at("/static/*").get(handle_get_static);
     app.listen("127.0.0.1:9876").await?;
     Ok(())
 }
