dc72ae5c904eb1c5dfca8a9f213ce19b6a0f8118
js <c86j224s@gmail.com>
2019-12-29 18:30:47 +0900

    Rust_Tide: refactor main.rs

--- Rust_Tide/todo/src/main.rs
+++ Rust_Tide/todo/src/main.rs
@@ -12,14 +12,41 @@ use serde::{Deserialize, Serialize};
 use tide::{prelude::*, IntoResponse, Request, Response, ResultExt, Server};
 
 
-struct GlobalStorage {
-    database: Arc<Mutex<Database>>
+//==============================================================================
+struct GlobalState {
+    message_storage: MessageStorage,
+    static_file_server: StaticFileHandler
 }
 
 
-struct Database {
-    message_storage: HashMap<usize, Message>,
-    static_file_server: StaticFileHandler
+impl GlobalState {
+     async fn new(root_path: &str) -> std::io::Result<Self> {
+        Ok(Self {
+            message_storage: Default::default(),
+            static_file_server: StaticFileHandler::new(root_path).await?
+        })
+    }
+
+    fn get_message_storage(&self) -> MessageStorage {
+        self.message_storage.clone()
+    }
+
+    fn get_static_file_server(&self) -> StaticFileHandler {
+        self.static_file_server.clone()
+    }
+}
+
+
+//==============================================================================
+#[derive(Default)]
+struct MessageStorage {
+    inner: Arc<Mutex<MessageStorageInner>>
+}
+
+
+#[derive(Default)]
+struct MessageStorageInner {
+    mm: HashMap<usize, Message>,
 }
 
 
@@ -30,24 +57,13 @@ struct Message {
 }
 
 
-impl GlobalStorage {
-    async fn new(root_path: &str) -> std::io::Result<GlobalStorage> {
-        Ok(GlobalStorage {
-            database: Arc::new(Mutex::new(Database {
-                message_storage: Default::default(),
-                static_file_server: StaticFileHandler::new(root_path).await?
-            }))
-        })
-    }
-
-    fn clone(&self) -> GlobalStorage {
-        GlobalStorage {
-            database: self.database.clone()
-        }
+impl MessageStorage {
+    fn clone(&self) -> Self {
+        Self { inner: self.inner.clone() }
     }
 
     async fn set_async(&self, id: usize, msg: Message) {
-        let contents = &mut self.database.lock().unwrap().message_storage;
+        let contents = &mut self.inner.lock().unwrap().mm;
 
         if contents.contains_key(&id) {
             contents.entry(id).and_modify(|v| *v = msg);
@@ -58,7 +74,7 @@ impl GlobalStorage {
     }
 
     async fn get_async(&self, id: usize) -> Option<Message> {
-        let contents = &self.database.lock().unwrap().message_storage;
+        let contents = &self.inner.lock().unwrap().mm;
 
         match contents.get(&id) {
             Some(v) => Some(v.clone()),
@@ -72,103 +88,10 @@ impl GlobalStorage {
         println!("done...");
     }
 
-    async fn get_static_async(&self, input_path: &Path) -> std::io::Result<Arc<Mutex<String>>> {
-        let mut sfs = { self.database.lock().unwrap().static_file_server.clone() };
-
-        sfs.get_cached_file(input_path).await
-    }
-}
-
-async fn handle_msg_get(req: Request<GlobalStorage>) -> Response {
-    let id = match req.param("id").client_err() {
-        Ok(v) => v,
-        Err(e) => return e.into_response()
-    };
-
-    let cloned_state = { req.state().clone() };
-
-    if let Some(msg) = cloned_state.get_async(id).await {
-        match Response::new(StatusCode::OK.into()).body_json(&msg) {
-            Ok(v) => v,
-            _ => Response::new(StatusCode::INTERNAL_SERVER_ERROR.into())
-            
-        }
-    }
-    else {
-        Response::new(StatusCode::NOT_FOUND.into())
-    }
-}
-
-async fn handle_msg_post(mut req: Request<GlobalStorage>) -> Response { 
-    let msg = match req.body_json().await.client_err() {
-        Ok(v) => v,
-        Err(e) => {
-            println!("body json parse failed. e = {:#?}", e);
-            return e.into_response()
-        }
-    };
-    let id = match req.param("id").client_err() {
-        Ok(v) => v,
-        Err(e) => {
-            println!("param id is not found. {:#?}", e);
-            return e.into_response()
-        }
-    };
-
-    let cloned_state = { req.state().clone() };
-
-    cloned_state.set_async(id, msg).await;
-    Response::new(StatusCode::OK.into()).body_string("".to_owned())
-}
-
-async fn handle_delay(req: Request<GlobalStorage>) -> Response {
-    let cloned_state = {
-        req.state().clone()
-    };
-
-    cloned_state.delay().await;
-    Response::new(StatusCode::OK.into()).body_string("ok.".to_owned())
-}
-
-async fn handle_get_static(req: Request<GlobalStorage>) -> Response {
-    let input_path_buf = req.uri().path().to_owned();
-    let refined_path_string = format!("./static_files/{}", &input_path_buf[8..]);
-    let rel_path = Path::new(&refined_path_string);
-
-    let cloned_state = {
-        req.state().clone()
-    };
-
-    match cloned_state.get_static_async(&rel_path).await {
-        Ok(v) => {
-            Response::new(StatusCode::OK.into()).body_string(v.lock().unwrap().to_owned())
-        },
-        Err(e) => {
-            println!("get static async failed. {:#?}", e);
-            Response::new(StatusCode::INTERNAL_SERVER_ERROR.into())
-        }
-    }
-}
-
-async fn handle_index(req: Request<GlobalStorage>) -> Response {
-    let rel_path = Path::new("./static_files/index.html");
-
-    let cloned_state = {
-        req.state().clone()
-    };
-
-    match cloned_state.get_static_async(&rel_path).await {
-        Ok(v) => {
-            Response::new(StatusCode::OK.into()).body_string(v.lock().unwrap().to_owned()).set_mime(mime::TEXT_HTML_UTF_8)
-        },
-        Err(e) => {
-            println!("get index async failed. {:#?}", e);
-            Response::new(StatusCode::OK.into()).body_string(format!("Hello, Tide! There's no index page."))
-        }
-    }
 }
 
 
+//==============================================================================
 struct StaticFileHandler {
     inner: Arc<Mutex<StaticFileHandlerInner>>
 }
@@ -268,14 +191,101 @@ impl StaticFileHandler {
             None => Err(std::io::Error::from(std::io::ErrorKind::NotFound))
         }
     }
+
+    async fn get_static_async(&mut self, input_path: &Path) -> std::io::Result<Arc<Mutex<String>>> {
+        self.get_cached_file(input_path).await
+    }
 }
 
 
+//==============================================================================
+async fn handle_msg_get(req: Request<GlobalState>) -> Response {
+    let id = match req.param("id").client_err() {
+        Ok(v) => v,
+        Err(e) => return e.into_response()
+    };
+
+    let msgstore = req.state().get_message_storage();
+
+    if let Some(msg) = msgstore.get_async(id).await {
+        match Response::new(StatusCode::OK.into()).body_json(&msg) {
+            Ok(v) => v,
+            _ => Response::new(StatusCode::INTERNAL_SERVER_ERROR.into())
+            
+        }
+    }
+    else {
+        Response::new(StatusCode::NOT_FOUND.into())
+    }
+}
+
+async fn handle_msg_post(mut req: Request<GlobalState>) -> Response { 
+    let msg = match req.body_json().await.client_err() {
+        Ok(v) => v,
+        Err(e) => {
+            println!("body json parse failed. e = {:#?}", e);
+            return e.into_response()
+        }
+    };
+    let id = match req.param("id").client_err() {
+        Ok(v) => v,
+        Err(e) => {
+            println!("param id is not found. {:#?}", e);
+            return e.into_response()
+        }
+    };
+
+    let msgstore = req.state().get_message_storage();
+
+    msgstore.set_async(id, msg).await;
+    Response::new(StatusCode::OK.into()).body_string("".to_owned())
+}
+
+async fn handle_delay(req: Request<GlobalState>) -> Response {
+    let msgstore = req.state().get_message_storage();
+
+    msgstore.delay().await;
+    Response::new(StatusCode::OK.into()).body_string("ok.".to_owned())
+}
+
+async fn handle_get_static(req: Request<GlobalState>) -> Response {
+    let input_path_buf = req.uri().path().to_owned();
+    let refined_path_string = format!("./static_files/{}", &input_path_buf[8..]);
+    let rel_path = Path::new(&refined_path_string);
+
+    let mut sfs = req.state().get_static_file_server();
+
+    match sfs.get_static_async(&rel_path).await {
+        Ok(v) => {
+            Response::new(StatusCode::OK.into()).body_string(v.lock().unwrap().to_owned())
+        },
+        Err(e) => {
+            println!("get static async failed. {:#?}", e);
+            Response::new(StatusCode::INTERNAL_SERVER_ERROR.into())
+        }
+    }
+}
+
+async fn handle_index(req: Request<GlobalState>) -> Response {
+    let rel_path = Path::new("./static_files/index.html");
+
+    let mut sfs = req.state().get_static_file_server();
+
+    match sfs.get_static_async(&rel_path).await {
+        Ok(v) => {
+            Response::new(StatusCode::OK.into()).body_string(v.lock().unwrap().to_owned()).set_mime(mime::TEXT_HTML_UTF_8)
+        },
+        Err(e) => {
+            println!("get index async failed. {:#?}", e);
+            Response::new(StatusCode::OK.into()).body_string(format!("Hello, Tide! There's no index page."))
+        }
+    }
+}
 
 
 #[async_std::main]
 async fn main() -> std::io::Result<()> {
-    let database = GlobalStorage::new("./static_files/").await?;
+    let database = GlobalState::new("./static_files/").await?;
 
     let mut app = Server::with_state(database);
     //app.at("/").get(|_| async move { "Hello, world!" });
