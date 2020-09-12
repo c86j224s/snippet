45febdd78749d31737173193c75fad390bf64609
jinsung <c86j224s@gmail.com>
2020-09-12 18:50:43 +0900

    webrtc_book2013: working..

--- WebRtc_Book2013/SigSrv/main.py
+++ WebRtc_Book2013/SigSrv/main.py
@@ -5,6 +5,7 @@
 # flask + socketio : https://yumere.tistory.com/53
 
 
+from base64 import b64encode
 import os
 from flask import Flask, render_template, session
 from flask_socketio import SocketIO, emit
@@ -27,7 +28,7 @@ def before_request():
         pass
     else:
         session['session'] = os.urandom(24)
-        session['username'] = 'user#'+str(session['session'])
+        session['username'] = 'user-{}'.format(b64encode(session['session']).decode('utf-8'))
         user_count += 1
 
 
@@ -52,6 +53,31 @@ def disconnect():
 @socketio.on('chat', namespace='/mynamespace')
 def chat(message):
     emit('chat_response', {'data': message['data'], 'username': session['username']}, broadcast=True)
+
+
+@socketio.on('caller_join', namspace='/mynamespace')
+def caller_join(message):
+    if not 'caller_token' in message:
+        print('error!!!')
+        emit('caller_join', {})
+        return
+
+    session['caller_token'] = message['caller_token']
+    # 보내면 안되지만 일단 그냥 보냄 --;;
+    emit('caller_join', {'caller_token': session['caller_token'], 'username': session['username']}, broadcast=True)
+
+
+@socketio.on('callee_join', namespace='/mynamespace')
+def callee_join(message):
+    if not 'caller_token' in message:
+        print('error!!!')
+        emit('callee_join', {})
+        return
+
+    session['caller_token'] = message['caller_token']
+    # 보내면 안되지만 일단 그냥 보냄 --;;
+    emit('callee_join', {'caller_token': session['caller_token'], 'username': session['username']}, broadcast=True)
+
 
 
 if __name__ == '__main__':
